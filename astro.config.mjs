3cbc10ba533ccef09af43bfd5b40921ef96bd6a1
Salvatore Dalba <salvadalba@tuta.io>
2025-09-26 15:14:28 +0200

    🔧 Update Astro config for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,36 @@
 // @ts-check
 
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import tailwind from '@astrojs/tailwind';
-import { defineConfig } from 'astro/config';
+     import mdx from '@astrojs/mdx';
+     import sitemap from '@astrojs/sitemap';
+     import tailwind from '@astrojs/tailwind';
+     import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({
-	site: 'https://nodaysidle-blog.vercel.app',
-	output: 'static',
-	integrations: [
-		mdx(),
-		tailwind({
-			applyBaseStyles: false
-		}),
-		sitemap({
-			changefreq: 'weekly',
-			priority: 0.7,
-			lastmod: new Date(),
-		})
-	],
-});
+     // https://astro.build/config
+     export default defineConfig({
+        site: 'https://salvadalba.github.io',
+        base: '/nodaysidle-blog',
+        output: 'static',
+        integrations: [
+                mdx(),
+                tailwind({
+                        applyBaseStyles: false
+                }),
+                sitemap({
+                        changefreq: 'weekly',
+                        priority: 0.7,
+                        lastmod: new Date(),
+                })
+        ],
+        build: {
+                assets: '_astro'
+        },
+        vite: {
+                build: {
+                        rollupOptions: {
+                                output: {
+                                        assetFileNames: '_astro/[name].[hash][extname]'
+                                }
+                        }
+                }
+        }
+     });
